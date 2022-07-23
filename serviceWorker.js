8dd782db6fcd88d9651193463e96772f1a3fd3a9
davidlorenzini <davidlorenzini@icloud.com>
2022-07-23 22:03:32 +0200

    Automated commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,23 +17,38 @@ self.addEventListener("install", installEvent => {
 
 self.addEventListener("fetch", event => {
   console.log(event)
+  cacheFirst(event)
   
+})
+
+function networkFirst(event) {
+  event.respondWith(caches.open(cacheName).then(cache => {
+    fetch(event.request).then(fetchedRes => {
+      cache.add(event.request, fetchedRes.clone())
+      return fetchedRes
+    }).catch(err => {
+      cache.match(event.request.url).then(cachedRes => cachedRes)
+    })
+  }))
+}
+
+function cacheFirst(event) {
   event.respondWith(caches.open(cacheName).then(cache => {
     // Go to the cache first
-    return cache.match(event.request.url).then(cachedResponse => {
+    return cache.match(event.request.url).then(cachedRes => {
       // Return a cached response if we have one
-      if (cachedResponse) {
-        return cachedResponse;
+      if (cachedRes) {
+        return cachedRes;
       }
 
       // Otherwise, hit the network
-      return fetch(event.request).then(fetchedResponse => {
+      return fetch(event.request).then(fetchedRes => {
         // Add the network response to the cache for later visits
-        cache.add(event.request, fetchedResponse.clone())
+        cache.add(event.request, fetchedRes.clone())
 
         // Return the network response
-        return fetchedResponse
+        return fetchedRes
       })
     })
   }))
-})
+}
